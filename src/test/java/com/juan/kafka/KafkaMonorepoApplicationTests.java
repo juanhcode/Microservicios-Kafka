3b4f546c426d978cf8c57b10757af194d6ec97fe
Juan Hoyos <juanhoyos1347@gmail.com>
2025-02-23 16:23:14 -0500

    project

---
+++ src/test/java/com/juan/kafka/KafkaMonorepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMonorepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
